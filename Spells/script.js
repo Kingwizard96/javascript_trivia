9e95227a35556434e461a825e452820a70b10b0c
Kingwizard96 <calvarado1996@gmail.com>
2023-08-16 07:48:19 -0500

    css and html have been written,javascript file contains pseudocode

--- Spells/script.js
+++ Spells/script.js
@@ -1,1 +1,6 @@
-console.log("hello,world");
+// create a timer that will start running when user click's start and can be manipulated by users answers 
+// if answered question correctly user gains an additional 5seconds
+// if answered incorrect user loses 10 seconds 
+// when game is over upload alias with score received 
+
+
